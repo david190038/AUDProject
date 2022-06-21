3d09bb028f3b6ec4a04214f21cc2ed899cfdd231
david190038 <vucakd190038@sus.htl-grieskirchen.at>
2022-06-21 21:57:00 +0200

    Image WebCrawler
    Image WebCrawler + Screenshot
    Link: https://gist.github.com/zita9999/e57e64a8eb550935c0b64d9c1763693c

---
+++ main.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+driver = webdriver.Chrome('C:/Web Scraping course/chromedriver.exe')
+driver.get('https://www.google.com/')
+
+box = driver.find_element_by_xpath('//*[@id="tsf"]/div[2]/div[1]/div[1]/div/div[2]/input')
+box.send_keys('giraffe')
+box.send_keys(Keys.ENTER)
+
+driver.find_element_by_xpath('//*[@id="hdtb-msb-vis"]/div[2]/a').click()
+
+# Will keep scrolling down the webpage until it cannot scroll no more
+last_height = driver.execute_script('return document.body.scrollHeight')
+while True:
+    driver.execute_script('window.scrollTo(0,document.body.scrollHeight)')
+    time.sleep(2)
+    new_height = driver.execute_script('return document.body.scrollHeight')
+    try:
+        driver.find_element_by_xpath('//*[@id="islmp"]/div/div/div/div/div[5]/input').click()
+        time.sleep(2)
+    except:
+        pass
+    if new_height == last_height:
+        break
+    last_height = new_height
+
+for i in range(1, 10):
+    try:
+        driver.find_element_by_xpath('//*[@id="islrg"]/div[1]/div[' + str(i) + ']/a[1]/div[1]/img').screenshot(
+            'C:\Web Scraping course\Downloading every image\giraffe (' + str(i) + ').png')
+    except:
+        pass
