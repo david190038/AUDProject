e33a733d8537587978f54adde232f749e1d808ca
david190038 <vucakd190038@sus.htl-grieskirchen.at>
2022-06-21 23:11:12 +0200

    Image Webcrawler fixed
    WebCrawler gefixt screenshots werden auch gemacht

--- main.py
+++ main.py
@@ -1,34 +1,62 @@
 from selenium import webdriver
+from selenium.common import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 import time
 
-driver = webdriver.Chrome('C:/Web Scraping course/chromedriver.exe')
-driver.get('https://www.google.com/')
+# selenium 3
+from webdriver_manager.chrome import ChromeDriverManager
 
-box = driver.find_element_by_xpath('//*[@id="tsf"]/div[2]/div[1]/div[1]/div/div[2]/input')
-box.send_keys('giraffe')
-box.send_keys(Keys.ENTER)
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.fullscreen_window()
 
-driver.find_element_by_xpath('//*[@id="hdtb-msb-vis"]/div[2]/a').click()
+##driver.get('https://www.google.com/search?q=giraffe')
+driver.get('https://www.google.com/search?site=&tbm=isch&source=hp&q=giraffe')
 
-# Will keep scrolling down the webpage until it cannot scroll no more
-last_height = driver.execute_script('return document.body.scrollHeight')
-while True:
-    driver.execute_script('window.scrollTo(0,document.body.scrollHeight)')
-    time.sleep(2)
-    new_height = driver.execute_script('return document.body.scrollHeight')
-    try:
-        driver.find_element_by_xpath('//*[@id="islmp"]/div/div/div/div/div[5]/input').click()it
+time.sleep(2)
+driver.fullscreen_window()
+
+## coockies
+try:
+    cookies_btn = driver.find_element_by_id('L2AGLb')
+    cookies_btn.click()
+except NoSuchElementException:  #spelling error making this code not work as expected
+    pass
+
+time.sleep(2)
+driver.fullscreen_window()
+
+try:
+    #Will keep scrolling down the webpage until it cannot scroll no more
+    last_height = driver.execute_script('return document.body.scrollHeight')
+
+    first_time = True
+    while True:
+        driver.execute_script('window.scrollTo(0,document.body.scrollHeight)')
         time.sleep(2)
-    except:
-        pass
-    if new_height == last_height:
-        break
-    last_height = new_height
+        new_height = driver.execute_script('return document.body.scrollHeight')
+        try:
+            driver.find_element_by_xpath('//*[@id="islmp"]/div/div/div/div/div[5]/input').click()
+            time.sleep(2)
+        except:
+            pass
+        if new_height == last_height:
+            ## end reached - last input is show more results cock
 
-for i in range(1, 10):
+            all_inputs = driver.find_elements_by_tag_name("input")
+            all_inputs.pop().click()
+
+            time.sleep(3)
+            if not first_time:
+                break
+            first_time = False
+
+        last_height = new_height
+
+except Exception:  #everything bc smart
+    pass
+
+for i in range(1, 270):
     try:
-        driver.find_element_by_xpath('//*[@id="islrg"]/div[1]/div[' + str(i) + ']/a[1]/div[1]/img').screenshot(
-            'C:\Web Scraping course\Downloading every image\giraffe (' + str(i) + ').png')
+        driver.find_element_by_xpath('//*[@id="islrg"]/div[1]/div['+str(i)+']/a[1]/div[1]/img').screenshot('D:\\test\\giraffe ('+str(i)+').jpg')
     except:
         pass
